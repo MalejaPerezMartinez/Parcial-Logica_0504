08192fde1aa6f440498ef3343397aed2020095a7
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-04-05 21:08:02 -0500

    Quito el condicional y agrego eldefault

--- P2_punto01/Program.cs
+++ P2_punto01/Program.cs
@@ -1,7 +1,11 @@
 ﻿internal class Program
 {
     public static void Main(string[] args)
+
     {
+        bool jugar1=true;
+        while(jugar1)
+        { 
         // adivina el numero Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
         //Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
         // Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
@@ -13,36 +17,38 @@
 
         jugadores = Convert.ToInt32(Console.ReadLine());
 
-        // Usar case para saber el rango de acuerdo a los jugadores
-        // if (jugadores < 2 && jugadores > 4) Console.Write("Numero de jugadores incorrectos, ingrese de nuevo");
-        // else
-        //{
+            switch (jugadores)
+            {
+                case 2:
+                    numfin = 50;
+                    break;
+                case 3:
+                    numfin = 100;
+                    break;
+                case 4:
+                    numfin = 200;
+                    break;
+                default:
+                    Console.WriteLine("Numero de jugadores incorrecto");
+                    continue;
+            }
+          // Generar un numero aleatorio
 
-        switch (jugadores)
-        {
-            case 2:
-                numfin = 50;
-                break;
-            case 3:
-                numfin = 100;
-                break;
-            case 4:
-                numfin = 200;
-                break;
+                Random random = new Random();
+                int aleato = random.Next(numini, numfin);
+                int turno = 0;
+                bool juego = false;
+
+                // Comienza el ciclo
+
+                switch (!juego) { Console.WriteLine("Turno del jugador:" + turno);
         }
-        //}
-        // Generar un numero aleatorio
+                 
+             
+     }
+ }
 
-        Random random = new Random();
-        int aleato = random.Next(numini, numfin);
-        int turno = 0;
-        bool juego = false;
 
-        // Comienza el ciclo
-        switch (!juego)
-        { Console.WriteLine("Turno del jugador:" + turno); }
-    }
-}
 
         
             
