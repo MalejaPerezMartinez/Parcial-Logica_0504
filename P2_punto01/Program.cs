f51e7c2f5699b9e507ffda18d05f9783f6a44ffa
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-04-05 21:18:24 -0500

    Cuarta actualizacion , estaba copiando mal la palabra while y por eso no me daba

--- P2_punto01/Program.cs
+++ P2_punto01/Program.cs
@@ -3,19 +3,19 @@
     public static void Main(string[] args)
 
     {
-        bool jugar1=true;
-        while(jugar1)
-        { 
-        // adivina el numero Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
-        //Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
-        // Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
-        int jugadores, numini, numfin;
-        numfin = 0; numini = 0;// inciar variables
-        Console.WriteLine("|--------- Juguemos a Adivina el numero-------|");
-        Console.WriteLine("|--Reglas: Solo puede jugar 2,3 o 4 personas--|");
-        Console.WriteLine("|------Ingresa el numero de participantes: ---|");
+        bool jugar1 = true;
+        while (jugar1)
+        {
+            // adivina el numero Si participan 2 jugadores, el número ℕ aleatorio se generará entre 0 y 50
+            //Si participan 3 jugadores, el número ℕ aleatorio se generará entre 0 y 100
+            // Si participan 4 jugadores, el número ℕ aleatorio se generará entre 0 y 200
+            int jugadores, numini, numfin, numing;
+            numfin = 0; numini = 0;// inciar variables
+            Console.WriteLine("|--------- Juguemos a Adivina el numero-------|");
+            Console.WriteLine("|--Reglas: Solo puede jugar 2,3 o 4 personas--|");
+            Console.WriteLine("|------Ingresa el numero de participantes: ---|");
 
-        jugadores = Convert.ToInt32(Console.ReadLine());
+            jugadores = Convert.ToInt32(Console.ReadLine());
 
             switch (jugadores)
             {
@@ -32,21 +32,46 @@
                     Console.WriteLine("Numero de jugadores incorrecto");
                     continue;
             }
-          // Generar un numero aleatorio
+            // Generar un numero aleatorio
 
-                Random random = new Random();
-                int aleato = random.Next(numini, numfin);
-                int turno = 0;
-                bool juego = false;
+            Random random = new Random();
+            int aleato = random.Next(numini, numfin);
+            int turno = 0;
+            bool juego2 = false;
 
-                // Comienza el ciclo
+            // Comienza el ciclo
 
-                switch (!juego) { Console.WriteLine("Turno del jugador:" + turno);
+            while (!juego2)
+
+            {
+                Console.Write("Turno del jugador:" + turno);
+                Console.Write("Digite el numero : ");
+                numing = Convert.ToInt32(Console.ReadLine());
+
+                // Condicionales para la validación
+
+                if(numing < aleato) 
+                {
+                    Console.Write("MENOR, turno para el siguiente jugador");
+                }
+
+                else if(numing > aleato)
+                {
+                    Console.Write("MAYOR, turno para el siguiente jugador");
+                }
+                else
+                {
+                    Console.Write("¡¡HAS GANADO!!");
+                    juego2=true;
+                }
+
+                
+            }
+
+
         }
-                 
-             
-     }
- }
+    }
+}
 
 
 
