a3da2e7a989d249565a0839912e3914cbd31feec
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-04-05 22:06:42 -0500

    Apartir del codigo inicial estoy reescribiendo para dos personas

---
+++ P2_punto02/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Variables
+
+        bool volver = true;
+        const double BONO = 0.4; //Snake Case:Notación para constantes.
+
+        while (volver)
+        {
+            // Variables
+
+            const double BONO = 0.4;
+            decimal aporteTotalp1 = 0, rendimientoTotalp1 = 0, bonoTotalp1 = 0,                prestamoTotalp1 = 0, multasTotalp1 = 0;
+            decimal aporteTotalp2 = 0, rendimientoTotalp2 = 0, bonoTotalp2 = 0, prestamoTotalp2 = 0, multasTotalp2 = 0;
+
+        }
+    }
+}
