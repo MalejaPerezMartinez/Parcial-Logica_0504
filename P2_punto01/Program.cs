bc00059e1626abc78f61e8d2d119f88881dd779d
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-04-05 20:50:25 -0500

    Se me presentan errores con lso parentesis del switch

--- P2_punto01/Program.cs
+++ P2_punto01/Program.cs
@@ -10,29 +10,59 @@
         Console.WriteLine("|--------- Juguemos a Adivina el numero-------|");
         Console.WriteLine("|--Reglas: Solo puede jugar 2,3 o 4 personas--|");
         Console.WriteLine("|------Ingresa el numero de participantes: ---|");
+
         jugadores = Convert.ToInt32(Console.ReadLine());
 
         // Usar case para saber el rango de acuerdo a los jugadores
-        if (jugadores < 2 && jugadores > 4) Console.Write("Numero de jugadores incorrectos, ingrese de nuevo");
-        else
+        // if (jugadores < 2 && jugadores > 4) Console.Write("Numero de jugadores incorrectos, ingrese de nuevo");
+        // else
+        //{
+
+        switch (jugadores)
         {
-
-            switch (jugadores)
-            {
-                case 2:
-                    numfin = 50;
-                    break;
-                case 3:
-                    numfin = 100;
-                    break;
-                case 4:
-                    numfin = 200;
-                    break;
-            }
-
-
-
-
+            case 2:
+                numfin = 50;
+                break;
+            case 3:
+                numfin = 100;
+                break;
+            case 4:
+                numfin = 200;
+                break;
         }
+        //}
+        // Generar un numero aleatorio
+
+        Random random = new Random();
+        int aleato = random.Next(numini, numfin);
+        int turno = 0;
+        bool juego = false;
+
+        // Comienza el ciclo
+        switch (!juego)
+        { Console.WriteLine("Turno del jugador:" + turno); }
     }
 }
+
+        
+            
+        
+
+    
+
+        
+
+    
+
+        
+            
+
+        
+
+       
+
+
+
+
+        
+    
